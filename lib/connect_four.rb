64941377fdd0150941436a53a2505639b7f6a38b
Cheong <xxlsxx1955@gmail.com>
2022-12-26 21:19:41 +0800

    Fix methods' behaviours
      Fix #board_horizontal_game_over? where check wasn't working as intended.
      Introduce new unicode characters as players' pieces.
      Improve processing round's flow, now it declare winner immediately if the game is over.

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -10,8 +10,8 @@ class ConnectFour
     @board = Array.new(row_amount) { Array.new(col_amount, ' ') }
     @col_insert_pos = Array.new(col_amount, row_amount - 1)
     @player_round = 'player1'
-    @player1_piece = '0'
-    @player2_piece = 'X'
+    @player1_piece = "\e[33m\u25cf\e[0m"
+    @player2_piece = "\e[34m\u25cf\e[0m"
   end
 
   def main
@@ -21,12 +21,17 @@ class ConnectFour
   end
 
   def progress_round
+    print_board
+
     until game_over?
-      print_board
+      print_current_player
 
       input = player_input
 
       insert_to_board_col(input)
+      print_board
+
+      break if game_over?
 
       advance_round
     end
@@ -97,7 +102,6 @@ class ConnectFour
     @col_insert_pos[col_index] == -1
   end
 
-  # FIXME: not working as intended
   def board_horizontal_game_over?
     window_size = 4
 
@@ -105,7 +109,7 @@ class ConnectFour
       (row.length - window_size).times do |left|
         curr_window = row[left..(left + (window_size - 1))]
 
-        return true if curr_window.all?(player1_piece || player2_piece)
+        return true if curr_window.all?(player1_piece) || curr_window.all?(player2_piece)
       end
     end
 
@@ -148,6 +152,10 @@ class ConnectFour
     puts " #{(0..6).to_a.join(' ')}"
   end
 
+  def print_current_player
+    puts "Current player: #{player_round == 'player1' ? 'player1' : 'player2'}"
+  end
+
   def draw_game
     puts 'There is no winner'
   end
@@ -166,11 +174,11 @@ class ConnectFour
     # FIXME
     # as winner declaration is defered until next round
     # as such, the winner will be last round player.
-    player_round == 'player1' ? 'player2' : 'player1'
+    player_round == 'player1' ? 'player1' : 'player2'
   end
 end
 
-# ConnectFour.new.main
+ConnectFour.new.main
 
 # TODO
 # Make the board prettier?
